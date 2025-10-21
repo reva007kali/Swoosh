bb1b3bd686162a1c926a57c1e60275662daa1246
reva007kali <le.revaldy@gmail.com>
2025-10-21 11:36:05 +0700

    commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->unique();
-            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade')->nullOnDelete();
+            $table->foreignId('role_id')->constrained('roles')->nullOnDelete();
             $table->string('password')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
